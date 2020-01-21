e1a3652b076f5afa95d1804533551c9706545d17
Đỗ Viết Linh <v.linhdv10@vinid.net>
2020-01-21 14:46:55 +0700

    ui menu category

---
+++ app/src/main/java/com/example/movietv/View/FragmentHome.kt
@@ -0,0 +1,62 @@
+package com.example.movietv.View
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentManager
+import androidx.fragment.app.FragmentPagerAdapter
+import androidx.viewpager.widget.ViewPager
+import com.example.movietv.R
+import com.google.android.material.tabs.TabLayout
+
+class FragmentHome : Fragment(){
+
+    private var tabLayout: TabLayout? = null
+    private var viewPager: ViewPager? = null
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        val view = inflater.inflate(R.layout.fragment_home, container, false)
+        tabLayout = view.findViewById(R.id.tabs) as TabLayout
+        viewPager = view.findViewById(R.id.viewpager) as ViewPager
+        viewPager!!.setAdapter(MyAdapter(fragmentManager))
+        tabLayout!!.post(Runnable { tabLayout!!.setupWithViewPager(viewPager) })
+        return view
+    }
+    private inner class MyAdapter(fm: FragmentManager?) : FragmentPagerAdapter(fm!!, BEHAVIOR_RESUME_ONLY_CURRENT_FRAGMENT) {
+        private val int_items = 5
+        override fun getItem(position: Int): Fragment {
+            var fragment: Fragment? = null
+            when (position) {
+                0 -> fragment = FragmentCategory()
+                1 -> fragment = FragmentCategory()
+                2 -> fragment = FragmentCategory()
+                3 -> fragment = FragmentCategory()
+                4 -> fragment = FragmentCategory()
+            }
+            return fragment!!
+        }
+
+        override fun getCount(): Int {
+            return int_items
+        }
+
+        override fun getPageTitle(position: Int): CharSequence? {
+            when (position) {
+                0 -> return "Popular"
+                1 -> return "Now Playing"
+                2 -> return "UpComing"
+                3 -> return "Top Rate"
+                4 -> return "Genres"
+
+
+            }
+            return null
+        }
+    }
+}
--- app/src/main/java/com/example/movietv/View/MainActivity.kt
+++ app/src/main/java/com/example/movietv/View/MainActivity.kt
@@ -2,12 +2,20 @@ package com.example.movietv.View
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.fragment.app.FragmentManager
+import androidx.fragment.app.FragmentTransaction
 import com.example.movietv.R
 
 class MainActivity : AppCompatActivity() {
-
+    private val TAG = "Home"
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val mFragmentManager: FragmentManager = supportFragmentManager
+        val fragmentTransaction: FragmentTransaction = mFragmentManager.beginTransaction()
+//        fragmentTransaction.addToBackStack(null)
+        fragmentTransaction.replace(
+            R.id.frame_home,
+            FragmentHome(), TAG).commit()
     }
 }
