f210400152dec28c9d896629aa1f1025c75ef657
Đỗ Viết Linh <v.linhdv10@vinid.net>
2020-01-21 11:05:56 +0700

    init base

---
+++ app/src/main/java/com/example/movietv/View/MainActivity.kt
@@ -0,0 +1,13 @@
+package com.example.movietv.View
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.movietv.R
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
